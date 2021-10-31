d06dee12cc26afcb6c97959fe64de8154410029f
Jimmy Hoang <dunghd296@gmail.com>
2021-11-01 00:48:56 +0700

    woking on sunny side desktop view

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -1,48 +1,53 @@
-let selectedItem = document.querySelector('.selected')
-inputData(selectedItem.innerHTML.toLowerCase())
+let selectedItem = document.querySelector(".selected");
+inputData(selectedItem.innerHTML.toLowerCase());
 
-function inputData (selectedTimeframe){
-    
-    fetch ('./data.json')
-            .then(resp => resp.json())
-            .then(function (data) {
-                for (let i=0; i < data.length; i++) {
-                    let itemID = data[i].title.toLowerCase() 
-                    itemID = itemID == 'self care'? 'self-care': itemID
-    
-                    let item = document.getElementById(itemID)
-                    let curentTime = data[i].timeframes[selectedTimeframe].current
-                    curentTime = curentTime == 0 ? curentTime + 'hr': curentTime + 'hrs'
-                    let previousTime = data[i].timeframes[selectedTimeframe].previous
-                    previousTime = previousTime == 0? previousTime + 'hr': previousTime + 'hrs'
-                    
-                    switch (selectedTimeframe){
-                        case 'daily':
-                            previousTime = 'Yesterday - ' + previousTime
-                            break
-                        case 'weekly':
-                            previousTime = 'Last week - ' + previousTime
-                            break
-                        case 'monthly':
-                            previousTime = 'Last month - ' + previousTime
-                            break
-                    }
+function inputData(selectedTimeframe) {
+  fetch("./data.json")
+    .then((resp) => resp.json())
+    .then(function (data) {
+        // loop through item
+      for (let i = 0; i < data.length; i++) {
+        // get item to work with
+        let itemID = data[i].title.toLowerCase();
+        itemID = itemID == "self care" ? "self-care" : itemID;
+        let item = document.getElementById(itemID);
 
+        let curentTime = data[i].timeframes[selectedTimeframe].current;
+        curentTime = curentTime == 0 ? curentTime + "hr" : curentTime + "hrs";
 
-                    item.querySelector('.card-infor__current-status').innerHTML = curentTime
-                    item.querySelector('.card-infor__history').innerHTML = previousTime
-                }
-            })
+        let previousTime = data[i].timeframes[selectedTimeframe].previous;
+        previousTime =
+          previousTime == 0 ? previousTime + "hr" : previousTime + "hrs";
+
+        switch (selectedTimeframe) {
+          case "daily":
+            previousTime = "Yesterday - " + previousTime;
+            break;
+          case "weekly":
+            previousTime = "Last week - " + previousTime;
+            break;
+          case "monthly":
+            previousTime = "Last month - " + previousTime;
+            break;
+        }
+
+        item.querySelector(".card-infor__current-status").innerHTML =
+          curentTime;
+        item.querySelector(".card-infor__history").innerHTML = previousTime;
+      }
+    });
 }
 
+// set onclick event to trigger change data
+let selectors = document.querySelectorAll(".period-selector");
 
-let selectors = document.querySelectorAll('.period-selector')
-
-selectors.forEach(selector => {
-    selector.addEventListener('click', () =>{
-        if (selector.classList.contains('selected')){return}
-        selectors.forEach(selector => selector.classList.remove('selected'))
-        selector.classList.add('selected')
-        inputData (selector.innerHTML.toLowerCase())
-    })
-})
+selectors.forEach((selector) => {
+  selector.addEventListener("click", () => {
+    if (selector.classList.contains("selected")) {
+      return;
+    }
+    selectors.forEach((selector) => selector.classList.remove("selected"));
+    selector.classList.add("selected");
+    inputData(selector.innerHTML.toLowerCase());
+  });
+});
